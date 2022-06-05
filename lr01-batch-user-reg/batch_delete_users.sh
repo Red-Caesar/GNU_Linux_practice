0a34b14d8e49bdbb1f6d5663013f480e719e76fc
Red-Caesar <suslovavd2002@mail.ru>
2022-06-05 15:55:01 +0300

    Fix name of backup and path of backup

--- lr01-batch-user-reg/batch_delete_users.sh
+++ lr01-batch-user-reg/batch_delete_users.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-date_for_b=$(date | awk -F ' ' {'print $1"_"$2"_"$3"_"$4"_"$5'})
+date_for_b=$(LANG=C date | awk -F ' ' {'print $1"_"$2"_"$3"_"$4"_"$5'})
 
 dir=`dirname $0`
 source_file="$dir/users_list.txt"
@@ -15,12 +15,12 @@ login=$(echo "$user_data" | awk -F ',' {'print $1'})
 if [ "$1" == "with-backup" ];
 then
 back_dir="/opt/backup_$date_for_b"
-tar -cf "$back_dir.tar" "/home/$login"
-echo "did backup"
+#user_path="$login.tar"
+tar -rf "$back_dir" "/home/$login" &>> /dev/null
+echo "did backup of $login"
 else
 echo "del without backup"
 fi
-echo ""
 
 deluser --remove-home "$login" &>> /dev/null
 
